3d2be26d099cdb52e42abcf0425eebc8ec5be5b2
Miguel Fialho <fialhomig@gmail.com>
2022-07-12 22:37:01 -0300

    Removendo o teste de verificação http(200) do arquivo de teste-pages.js

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,20 +1,14 @@
-const expect = require("chai").expect;
-const request = require("request");
+const expect = require('chai').expect;
+const request = require('request');
 
-describe("Status and content", function () {
-  describe("Main page", function () {
-    it("content", function (done) {
-      request("http://localhost:8888/", function (error, response, body) {
-        var test = "Hello World!!";
-        expect(test).to.be.a("String");
-        done();
-      });
-    });
-    it("status", function (done) {
-      request("http://localhost:8888/", function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
-  });
+describe('Status and content', function () {
+	describe('Main page', function () {
+		it('content', function (done) {
+			request('http://localhost:8888/', function (error, response, body) {
+				var test = 'Hello World!!';
+				expect(test).to.be.a('String');
+				done();
+			});
+		});
+	});
 });
